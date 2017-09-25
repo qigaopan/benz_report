ad883098f2b4d0bff4a5ade5f7412d50f800b2c4
Gaopan Qi <qigaopan2007@hotmail.com>
2017-09-25 18:09:17 +0800

    initial benz report package structure

---
+++ benz_report/__init__.py
@@ -0,0 +1,22 @@
+'''
+Benz_Report: Main module
+
+Copyright 2017, Gaopan Qi
+Licensed under MIT.
+'''
+
+
+def main():
+    '''
+    Main function of the boilerplate code is the entry point of the 'benz_report' executable script (defined in setup.py).
+    
+    Use doctests, those are very helpful.
+    
+    >>> main()
+    Hello
+    >>> 2 + 2
+    4
+    '''
+    
+    print("Hello")
+
---
+++ tests/basic_test.py
@@ -0,0 +1,13 @@
+'''
+Benz_Report: Test module.
+
+Meant for use with py.test.
+Write each test as a function named test_<something>.
+Read more here: http://pytest.org/
+
+Copyright 2017, Gaopan Qi
+Licensed under MIT
+'''
+
+def test_example():
+    assert True
